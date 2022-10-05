6532225aaf8c6bd3603995142a30bd301a2eac75
Shubhamlmp <shubhunic@gmail.com>
2022-10-05 13:34:41 +0530

    added demo code

---
+++ C++/sayHelloWithC++.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Create a string variable
+    string s;
+    // Taking input from user
+    cin >> s;
+    cout << "Hello," << " " << s ;
+}
