bd9395b14d69e5e37cd7659e242bd4da075bbdba
chiragkapoor20 <78134726+chiragkapoor20@users.noreply.github.com>
2022-10-07 06:21:45 +0530

    Create Area of a Circle.cpp
     this program is completely run

---
+++ C++/Area of a Circle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double pi= 3.141592653;
+    
+    double r;
+    cin>>r;
+    
+    double Area=pi*(r*r);
+    
+     cout << fixed << setprecision(9)<<Area;
+     // your code.
+}
