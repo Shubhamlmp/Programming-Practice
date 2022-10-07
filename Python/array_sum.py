9976b8fbdb7ae6a699560019d6ccc2be8f36fd5f
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-10-07 22:57:53 +0530

    update array_sum.py
    Added comments

--- Python/array_sum.py
+++ Python/array_sum.py
@@ -1,9 +1,12 @@
+#importing array as name of arr
 import array as arr
+#defining integer type as i and storing it under[]
 a=arr.array('i',[23,45,67,23,12])
-
+#initializing counter as result =0
 result=0
+#running for loop to loop through values in an array
 for values in a:
-     result+=values
+     result+=values #incrementing the value as looping continously through values in an array
 
-
+#printing the result
 print(result)
