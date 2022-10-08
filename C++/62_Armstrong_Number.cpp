c3c4c3b6741352d3392dd71a3e28e7c26332be6b
zoraizasif <zoraizasif9@gmail.com>
2022-10-09 01:25:37 +0500

    armstrong number program

---
+++ C++/62_Armstrong_Number.cpp
@@ -0,0 +1,32 @@
+// C++ program to check whether the number is Armstrong or not
+// Armstrong number is a number that is equal to the sum of cubes of its digits.
+// For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+
+// 371 = (3*3*3)+(7*7*7)+(1*1*1)    
+// where:    
+// (3*3*3)=27    
+// (7*7*7)=343    
+// (1*1*1)=1    
+// So:    
+// 27+343+1=371   
+
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+    int n,r,sum=0,temp;    
+    cout<<"Enter the Number=  ";    
+    cin>>n;    
+    temp=n;    
+    while(n>0) {
+        r=n%10;    
+        sum=sum+(r*r*r);    
+        n=n/10;
+    }    
+    if(temp==sum)    
+        cout<<"Armstrong Number."<<endl;    
+    else    
+        cout<<"Not Armstrong Number."<<endl;   
+    return 0;  
+}  
