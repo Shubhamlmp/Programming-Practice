b488d11ac166eff39ef199a2a9d0f13037e3dfed
Aishika3 <96141359+Aishika3@users.noreply.github.com>
2022-10-09 20:57:33 +0530

    Create freq.cpp

---
+++ C++/freq.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    map<int,int>freq;
+    for(int i=0;i<n;i++)
+    {
+        freq[arr[i]]++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(freq[arr[i]]!=0)
+        {
+            cout<<freq[arr[i]]<<endl;
+            freq[arr[i]]=0;
+        }
+    }
+    return 0;
+}
