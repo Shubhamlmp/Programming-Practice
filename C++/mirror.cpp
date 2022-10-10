070a63462ffcb9fc677821f28a035b261d21a5d0
Aishika3 <96141359+Aishika3@users.noreply.github.com>
2022-10-10 22:11:02 +0530

    Update mirror.cpp

--- C++/mirror.cpp
+++ C++/mirror.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int main()
 {
     int n,t;
-    cin>>n>>t;
+    cin>>n>>t;//initialising n and t(no of test case)
     while(t--)
     {
-        int arr[n];
+        int arr[n];//creating array of size n
         for(int i=0;i<n;i++)
         {
             cin>>arr[i];
         }
         int j=n-1,i=0;
+        //reversing the array rather making its mirror image
         while(i<=j)
         {
             int temp=arr[i];
@@ -21,6 +22,7 @@ int main()
             i++;
             j--;
         }
+        //printing the array
         for(int i=0;i<n;i++)
         {
             cout<<arr[i]<<" ";
