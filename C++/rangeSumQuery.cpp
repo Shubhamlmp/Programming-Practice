d8cf0c1f8a56730866d554704bba5a2163b62986
Aishika3 <96141359+Aishika3@users.noreply.github.com>
2022-10-11 18:31:38 +0530

    Update rangeSumQuery.cpp

--- C++/rangeSumQuery.cpp
+++ C++/rangeSumQuery.cpp
@@ -3,23 +3,29 @@
 using namespace std;
 int main()
 {
-    long long int n,q;//initialising n and q
+    long long int n,q;
     cin>>n>>q;
-    long long int arr[n];//initialising array of size n
+    long long int arr1[n];
+    long long int arr2[n];
     for(int i=0;i<n;i++)
     {
-        cin>>arr[i];
+        cin>>arr1[i];
+        if(i==0)
+        {
+            arr2[i]=arr1[i];
+        }
+        else{
+            arr2[i]=arr1[i]+arr2[i-1];
+        }
     }
     while(q--)
     {
-        long long int l,r;//input of lower limit and upper limit ie the range in which the sum is being calculated
+        long long int l,r;
         cin>>l>>r;
-        long long int sum=0;
-        for(int i=l-1;i<r;i++)
-        {
-            sum=sum+arr[i];
-        }
-        cout<<sum<<endl;//sum displayed
+        l--;
+        r--;
+        long long sum=arr2[r]-arr2[l]+arr1[l];
+        cout<<sum<<endl;
     }
     return 0;
 }
