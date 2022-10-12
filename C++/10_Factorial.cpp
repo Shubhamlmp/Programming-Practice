44c89e19ce9c681d040fd77ff68c4059eb0794e2
Shraggus <94532831+Shraggus@users.noreply.github.com>
2022-10-12 17:01:14 +0530

    Update 10_Factorial.cpp

--- C++/10_Factorial.cpp
+++ C++/10_Factorial.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int factorial(int n){
     if(n==0) //base case
-    return 1;
+      return 1;
     else
-    return n*factorial(n-1); //recursion statement
+      return n*factorial(n-1); //recursion statement to repeatedly multiply the number with a number 1 less than it, until reaching 0
 }
 
 int main(){
