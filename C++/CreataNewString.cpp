ca9521bd446ccde20e3d7219a68528571f14eae0
Abhi7717 <95903021+Abhi7717@users.noreply.github.com>
2022-10-16 23:36:42 +0530

    Create CreataNewString.cpp
    creat a new string program has completed.

---
+++ C++/CreataNewString.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    string str,st;
+    int i,m,n;
+    cout << "enter the string"<<endl;
+    cin>>str;
+    cout << "enter the string"<<endl;
+    cin>>st;
+    m=str.size();
+    n=st.size();
+    cout<<m<<" "<<n;
+    cout<<"\n"<<str<<" "<<st;
+    
+    return 0;
+}
