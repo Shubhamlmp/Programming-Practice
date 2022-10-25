4ad28355462b067b6142fe62be99ac93634dfebe
mili srivastava <milisrivastava@gmail.com>
2022-10-25 10:57:30 +0530

    easy method to practice

--- C++/binarysearch.cpp
+++ C++/binarysearch.cpp
@@ -1,37 +1,59 @@
+/* what is binary search?
+Binary Search Algorithm: The basic steps to perform Binary Search are:
+
+Begin with the mid element of the whole array as a search key.
+If the value of the search key is equal to the item then return an index of the search key.
+Or if the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half.
+Otherwise, narrow it to the upper half.
+Repeatedly check from the second point until the value is found or the interval is empty.
+
+
+time complexity of binary search is O(log n)
+
+Easiest iterative method to practice binary search
+*/
+
+
+
 #include<iostream>
-
 using namespace std;
+int binarysearch(int arr[], int n, int k){
+    int s=0;
+    int e=n;
+    
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        if(arr[mid] == k){
+            return mid;
 
-void input(int input[],int size)
-{
-    cout<<"enter elements of arry in sorted order\n"
-    for(int i=0;i<size;i++)
-    {
-        cin>>input[i];
-    }
-}
-
-void sort(int arr[]; int n)
-{
-    int temp;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i]>arr[i+1])
-        {
-            temp=arr[i];
-            arr[i]=arr[i+1];
-            arr[i+1]=temp;
         }
+        else if(arr[mid]>k){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+       
     }
-}
+    return -1;
+   
 
+    
+}
 int main()
 {
-int arr[1000],n;
-cout<<"enter size of arry\n";
-cin>>n;
-input(arr,n);
-
-
-return 0;
+    int n;
+    cout<<"enter the size of array :"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<" Enter the elements of array in sorted order"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<" Enter the number you want to search :"<<endl;
+    int k;
+    cin>>k;
+    cout<<"the number is present at the index: "<<endl;
+    cout<< binarysearch(arr, n, k);
+    return 0;
 }
