aae87472075afb3a534ef8ae7cc6fe1343158a73
Labi <73027342+labiiiii@users.noreply.github.com>
2022-10-31 21:54:08 +0530

    Create OverloadingOperator.java

---
+++ OverloadingOperator.java
@@ -0,0 +1,14 @@
+public class OverloadingOperator {
+
+	public static void main(String[] args) {
+	  int result=10+20;
+	  System.out.println(result);
+	  
+	  String s1="Aliya";
+	  String s2=" Kalam";
+	  
+	  System.out.println(s1+s2);
+
+	}
+
+}
